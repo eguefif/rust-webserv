9b366da2e722e501b7a5231adca4799a856b713a
Emmanuel Guefif <eguefif@fastmail.com>
2024-12-25 12:02:22 -0500

    Extract http_handler from main

---
+++ src/http_handler.rs
@@ -0,0 +1,55 @@
+use crate::parsers::http::HttpPacket;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
+
+pub async fn handle_packet(raw_packet: &Vec<u8>, socket: &mut TcpStream) -> bool {
+    if raw_packet.len() < 4 || !is_header_receive(raw_packet) {
+        return true;
+    }
+    let packet = String::from_utf8(raw_packet.clone())
+        .expect("Error while converting bytes into string (reading)");
+    let http_packet = HttpPacket::new(&packet);
+    eprintln!("Http request line : {:?}", http_packet.request_line);
+    eprintln!("Http headers : {:?}", http_packet.headers);
+    if is_connection_closing(&http_packet) {
+        return false;
+    }
+    let response = create_response(String::from("Hello, World!"));
+    socket
+        .write_all(response.as_bytes())
+        .await
+        .expect("Error while writing in socket");
+    return true;
+}
+
+fn is_connection_closing(http_packet: &HttpPacket) -> bool {
+    if let Some(connection) = http_packet.headers.get("connection") {
+        return connection != "keep-alive";
+    }
+    true
+}
+
+fn is_header_receive(raw_packet: &Vec<u8>) -> bool {
+    let idx = raw_packet.len() - 4;
+    raw_packet[idx] == 13
+        && raw_packet[idx + 1] == 10
+        && raw_packet[idx + 2] == 13
+        && raw_packet[idx + 3] == 10
+}
+
+fn create_response(body: String) -> String {
+    let mut response = String::new();
+    response.push_str("HTTP/1.1 200 OK\r\n");
+    response.push_str(format!("Date: {}\r\n", get_time().as_str()).as_str());
+    response.push_str(format!("Content-Length: {}\r\n", body.len()).as_str());
+    response.push_str("Server: rust-webserv");
+    response.push_str("\r\n\r\n");
+    response.push_str(body.as_str());
+
+    response
+}
+
+fn get_time() -> String {
+    // TODO: set date properly
+    String::from("Wed, 25 Dec 2024 15:10:17 GMT")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use crate::parsers::http::HttpPacket;
+use crate::http_handler::handle_packet;
 use std::error::Error;
-use tokio::io::AsyncWriteExt;
 use tokio::net::{TcpListener, TcpStream};
 
+pub mod http_handler;
 pub mod parsers;
 
 #[tokio::main]
@@ -16,46 +16,23 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
 async fn process(socket: &mut TcpStream) {
     let mut raw_packet: Vec<u8> = vec![];
-    let mut nbytes = 0;
     loop {
         if let Ok(_) = socket.readable().await {
             let mut buff = [0; 10];
             match socket.try_read(&mut buff[..]) {
-                Ok(n) => nbytes += n,
+                Ok(n) => {
+                    if n == 0 {
+                        eprintln!("Socket closed by client");
+                    }
+                    raw_packet.extend_from_slice(&buff);
+                    if !handle_packet(&raw_packet, socket).await {
+                        eprintln!("Connection closed");
+                    }
+                }
                 Err(_) => break,
             }
-            raw_packet.extend_from_slice(&buff);
         } else {
             eprintln!("Socket not readable");
         }
     }
-    let packet =
-        String::from_utf8(raw_packet).expect("Error while converting bytes into string (reading)");
-    let http_packet = HttpPacket::new(&packet);
-    eprintln!("Receive: {} bytes", nbytes);
-    eprintln!("Http request line : {:?}", http_packet.request_line);
-    eprintln!("Http headers : {:?}", http_packet.headers);
-    let response = create_response(http_packet, String::from("Hello, World"));
-    eprintln!("sending: {:?}", response.as_bytes());
-    socket
-        .write_all(response.as_bytes())
-        .await
-        .expect("Error while writing in socket");
-}
-
-fn create_response(http_packet: HttpPacket, body: String) -> String {
-    let mut response = String::new();
-    response.push_str("HTTP/1.1 200 OK\r\n");
-    response.push_str(format!("Date: {}\r\n", get_time().as_str()).as_str());
-    response.push_str(format!("Content-Length: {}, \r\n", body.len()).as_str());
-    response.push_str("Server: rust-webserv");
-    response.push_str("\r\n\r\n");
-    response.push_str(body.as_str());
-
-    response
-}
-
-fn get_time() -> String {
-    // TODO: set date properly
-    String::from("Wed, 25 Dec 2024 15:10:17 GMT")
 }
