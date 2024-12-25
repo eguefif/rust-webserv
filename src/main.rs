a05ddb10fff90041f4b4541ac0b3b91b6213ea08
Emmanuel Guefif <eguefif@fastmail.com>
2024-12-25 11:35:49 -0500

    Refactor create response to take a body

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ async fn process(socket: &mut TcpStream) {
     eprintln!("Receive: {} bytes", nbytes);
     eprintln!("Http request line : {:?}", http_packet.request_line);
     eprintln!("Http headers : {:?}", http_packet.headers);
-    let response = create_response(http_packet);
+    let response = create_response(http_packet, String::from("Hello, World"));
     eprintln!("sending: {:?}", response.as_bytes());
     socket
         .write_all(response.as_bytes())
@@ -43,18 +43,19 @@ async fn process(socket: &mut TcpStream) {
         .expect("Error while writing in socket");
 }
 
-fn create_response(http_packet: HttpPacket) -> String {
+fn create_response(http_packet: HttpPacket, body: String) -> String {
     let mut response = String::new();
     response.push_str("HTTP/1.1 200 OK\r\n");
     response.push_str(format!("Date: {}\r\n", get_time().as_str()).as_str());
-    response.push_str("Content-Length: 12\r\n");
+    response.push_str(format!("Content-Length: {}, \r\n", body.len()).as_str());
     response.push_str("Server: rust-webserv");
     response.push_str("\r\n\r\n");
-    response.push_str("Hello, World");
+    response.push_str(body.as_str());
 
     response
 }
 
 fn get_time() -> String {
+    // TODO: set date properly
     String::from("Wed, 25 Dec 2024 15:10:17 GMT")
 }
