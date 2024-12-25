404d4996f0dc12d0cfc693bb5e87e0f5dacf3fdb
Emmanuel Guefif <eguefif@fastmail.com>
2024-12-25 13:46:54 -0500

    Add cancellation token a

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,49 @@
 use crate::http_handler::handle_packet;
-use std::error::Error;
 use tokio::net::{TcpListener, TcpStream};
+use tokio::select;
+use tokio::signal;
+use tokio_util::sync::CancellationToken;
 
 pub mod http_handler;
 pub mod parsers;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let server = TcpListener::bind("127.0.0.1:8888").await?;
-    eprintln!("Listening on 127.0.0.1:8888");
+async fn main() {
+    let token = CancellationToken::new();
+    let cloned_token = token.clone();
+    let w = tokio::spawn(async move { worker(cloned_token).await });
     loop {
-        let (mut socket, _) = server.accept().await?;
-        tokio::spawn(async move { process(&mut socket).await });
+        match signal::ctrl_c().await {
+            Ok(_) => {
+                token.cancel();
+                eprintln!("Gracefully shutdown");
+                break;
+            }
+            _ => {}
+        }
     }
 }
 
-async fn process(socket: &mut TcpStream) {
+async fn worker(token: CancellationToken) {
+    if let Ok(server) = TcpListener::bind("127.0.0.1:8888").await {
+        eprintln!("Listening on 127.0.0.1:8888");
+        let mut tasks = Vec::new();
+        loop {
+            let cloned_token = token.clone();
+            if let Ok((mut socket, _)) = server.accept().await {
+                tasks.push(tokio::spawn(async move {
+                    process(&mut socket, cloned_token).await
+                }));
+            } else {
+                eprintln!("Impossible to accept socket on 127.0.0.:8888");
+            }
+        }
+    } else {
+        eprintln!("Impossible to listen on 127.0.0.1:8888");
+    }
+}
+
+async fn process(socket: &mut TcpStream, token: CancellationToken) {
     let mut raw_packet: Vec<u8> = vec![];
     loop {
         if let Ok(_) = socket.readable().await {
@@ -24,16 +52,18 @@ async fn process(socket: &mut TcpStream) {
                 Ok(n) => {
                     if n == 0 {
                         eprintln!("Socket closed by client");
+                        break;
                     }
                     raw_packet.extend_from_slice(&buff);
                     if !handle_packet(&raw_packet, socket).await {
                         eprintln!("Connection closed");
                     }
                 }
-                Err(_) => break,
+                Err(_) => raw_packet.clear(),
             }
         } else {
             eprintln!("Socket not readable");
         }
     }
+    eprintln!("Closing socket");
 }
